20dbe20c314b17a7aaf0ab8822a4438609f6cc36
MilinPagare <pagare.milin1996@gmail.com>
2023-12-23 12:50:31 +0530

    offline type ahead completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
-import Home from './components/Home';
+import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <Home/>
+      <h1>Type Any Country Name</h1>
+      <Home />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,47 +1,56 @@
 import React, { useEffect, useState } from "react";
-import {Box,Grid,ImageList,ImageListItem} from "@mui/material";
+import { allCountries } from "./Countries";
+import "./Home.css";
 
 const Home = () => {
-    const [images, setImages] = useState([]);
-    const [imgToDisplay, setImgToDisplay] = useState([]);
-    const [load, setLoad] = useState(20);
-    const [toAdd, setToAdd] = useState(0);
+  const [toShow, setToShow] = useState([]);
+  const [search, setSearch] = useState("");
 
-    const getImages = async() => {
-        const result = await fetch("https://jsonplaceholder.typicode.com/photos");
-        const temp = await result.json();
-        setImages(temp);
-        setImgToDisplay(temp.slice(0,load));
-        setToAdd(load);
+  const handleSearch = (e) => {
+    if (e.target.value === "") {
+      setToShow([]);
+    } else {
+      setToShow(
+        allCountries.filter((item) =>
+          item.toLowerCase().includes(e.target.value.toLowerCase())
+        )
+      );
     }
+  };
 
-    const handleScroll = () => {
-        const userScrollHeight = window.innerHeight+window.scrollY;
-        const windowBottomHeight = document.documentElement.offsetHeight;
-        if(userScrollHeight>=windowBottomHeight){
-            // getImages();
-            setImgToDisplay(images.slice(toAdd, toAdd+load));
-            setToAdd(toAdd+load);
-        }
-    }
+  const handleClick = (country) => {
+    setSearch(country);
+    setToShow([]);
+  };
 
-    useEffect(()=>{
-        getImages();
-        window.addEventListener('scroll', handleScroll);
-    },[]);
-// https://jsonplaceholder.typicode.com/photos
-// https://picsum.photos/v2/list
-    return (
-        <Box>
-            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                {imgToDisplay.map((item) => (
-                    <Grid item xs={2} sm={4} md={4} key={item.id}>
-                        <h1>hello</h1>
-                    </Grid>
-                ))}
-            </Grid>
-        </Box>
-    );
-}
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value);
+          handleSearch(e);
+        }}
+        className="search_box"
+      />
+      <div className="country_list">
+        {toShow.map((item, index) => (
+          <div
+            key={index}
+            className="country"
+            onClick={() => {
+              handleClick(item);
+            }}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
